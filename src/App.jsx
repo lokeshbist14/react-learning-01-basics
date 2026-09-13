5aea03cf0a1e8e200c468472a71659a90fc170e2
Lokesh Bist <bistlokesh729@gmail.com>
2026-09-13 17:44:37 +0545

    React basics first lesson

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>My First React Website</h1>
+      <p>I am learning  React</p>
+    </div>
+  )
+}
+
+export default App
